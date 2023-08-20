d42dbb960d5f4e185ed22599731a5a2dac75cdac
lulla-by <thfl3363@hanmail.net>
2023-08-20 21:20:33 +0900

    프로그래밍 방식 네비게이션 -useRouter의 push메서드를 사용 이동

--- 01.NextJS_Summary2/components/meetups/MeetupItem.js
+++ 01.NextJS_Summary2/components/meetups/MeetupItem.js
@@ -1,7 +1,13 @@
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
+import { useRouter } from 'next/router';
 
 function MeetupItem(props) {
+  const router = useRouter();
+  function showDetailsHandler() {
+    // 페이지 스택에 새 페이지를 푸시, Link 컴포넌트를 사용했을 때와 동일한 동작을 구현
+    router.push(`/${props.id}`)
+  };
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +19,7 @@ function MeetupItem(props) {
           <address>{props.address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={showDetailsHandler}>Show Details</button>
         </div>
       </Card>
     </li>
