5c8e8c79bb489d8e7ebf1737c323706845a0e137
lulla-by <thfl3363@hanmail.net>
2023-08-20 16:24:26 +0900

    동적 라우트 데이터 추가하기-useRouter - Next팀에서 개발한 맞춤형 훅 - URL에 인코딩된 값에 접근하게 해주는 메서드인 query 객체로 접근

--- 00.NextJS_Summary/pages/news/[newsId].js
+++ 00.NextJS_Summary/pages/news/[newsId].js
@@ -1,7 +1,14 @@
+import {useRouter} from 'next/router'
 // our-domain.com/news/[newsId] 접속
 
 function DetailPage (){
+  const router = useRouter();
+  const newsId = router.query.newsId;
 
+  //send a request to the backend API
+  // to fetch the news item with newsId
+
+  
   return <h1> This is Detail Page</h1>
 }
 
