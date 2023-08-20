d631c11e6392ab8151cc8a1b7a0e00325a3428d6
lulla-by <thfl3363@hanmail.net>
2023-08-20 15:55:55 +0900

    초기 세팅 및 첫번째 페이지 추가하기

---
+++ 00.NextJS_Summary/pages/index.js
@@ -0,0 +1,8 @@
+// 이 웹사이트의 루트페이지
+// our-domain.com/ 접속
+function HomePage (){
+
+  return <h1>This is Home Page</h1>
+}
+
+export default HomePage;
---
+++ 00.NextJS_Summary/pages/news.js
@@ -0,0 +1,9 @@
+// 도메인/news에 접속을 요청할 때 표시되는 페이지
+// our-domain.com/news 접속 => news.js파일 불러옴
+
+function NewsPage (){
+
+  return <h1> This is News Page</h1>
+}
+
+export default NewsPage;
