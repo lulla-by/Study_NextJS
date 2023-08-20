b87255196d1cf221e50e7b85fd424b2df954f26b
lulla-by <thfl3363@hanmail.net>
2023-08-20 21:43:00 +0900

    커스텀 컴포넌트 추가하기 & CSS 모듈을 통한 스타일링

--- 01.NextJS_Summary2/pages/[meetupId]/index.js
+++ 01.NextJS_Summary2/pages/[meetupId]/index.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react'
 import { useRouter } from 'next/router';
+import MeetupDetail from '../../components/meetups/MeetupDetails';
 
-const Detail = () => {
-  const id = useRouter().query.meetupId;
-  return<Fragment>
-    <div>router id: {id}</div>
-  </Fragment>
-  
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail image="https://cdn.shopify.com/s/files/1/0057/4439/4355/files/A-DSC_6996.jpg?v=1626384797" title="first meetup" address="London" description="description" />
+  )
+
 }
 
-export default Detail;
+export default MeetupDetails;
