f05c2a73d6b8715a6f07f325580f46ac1244f5b8
lulla-by <thfl3363@hanmail.net>
2023-08-21 16:32:07 +0900

    getStaticProps에서 동적 경로 매개변수 작업 - getStaticProps의 context매개변수 속 parmas key접근

--- 01.NextJS_Summary2/pages/[meetupId]/index.js
+++ 01.NextJS_Summary2/pages/[meetupId]/index.js
@@ -2,11 +2,30 @@ import React, { Fragment } from 'react'
 import { useRouter } from 'next/router';
 import MeetupDetail from '../../components/meetups/MeetupDetails';
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
+  console.log(props)
   return (
     <MeetupDetail image="https://cdn.shopify.com/s/files/1/0057/4439/4355/files/A-DSC_6996.jpg?v=1626384797" title="first meetup" address="London" description="description" />
   )
 
 }
 
+
+export async function getStaticProps(context) {
+  //useRouter는 함수 컴포넌트에서만 사용가능, getStaticProps에서는 사용x
+  // => context매개변수 사용 가능, getServerSideProps와 달리 req, res는 없지만 params는 키는 사용가능
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image: "https://cdn.shopify.com/s/files/1/0057/4439/4355/files/A-DSC_6996.jpg?v=1626384797",
+        id: meetupId,
+        title: "first meetup",
+        address: "London",
+        description: "description"
+      }
+    }
+  }
+}
 export default MeetupDetails;
