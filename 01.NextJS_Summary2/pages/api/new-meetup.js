53496a2df3ad9b95d050d068bb30143bba4b666d
lulla-by <thfl3363@hanmail.net>
2023-08-22 13:13:06 +0900

    API라우트로 HTTP전송

--- 01.NextJS_Summary2/pages/api/new-meetup.js
+++ 01.NextJS_Summary2/pages/api/new-meetup.js
@@ -2,22 +2,23 @@ import { MongoClient } from "mongodb"
 // 서버 사이드 코드를 지니고 있는 함수를 정의
 // 클라이언트가 아닌 서버 사이드에서만 실행이 가능함, 클라이언트에 노출x
 
+
+// NextJS는 요청이 이 경로에(/api/new-meetup) 도달하면 handler 함수를 트리거
 async function handler(req, res) {
+
   if (req.method === "POST") {
     const data = req.body;
     // client에서 실행되면 안되는 코드 => 보안상의 문제
-  const clinet = await MongoClent.connect(`mongodb+srv://lee:${process.env.REACT_MONGODB}@nexttjs.hquh822.mongodb.net/meetups?retryWrites=true&w=majority`)
-    const db = clinet.db()
+    const client = await MongoClient.connect(process.env.REACT_APP_MONGODB)
+    const db = client.db()
 
-    const meetusCollection = db.colletion("meetups")
-    const result = await meetusCollection.insertOne({data})
-    console.log(result)
-    clinet.close()
+    const meetupsCollection = db.collection("meetups")
+    const result = await meetupsCollection.insertOne({ data })
+    client.close()
 
-    res.status(201).json({message:"Meetup inserted!"})
+    res.status(201).json({ message: "Meetup inserted!" })
 
 
-    res
   }
 }
 
--- 01.NextJS_Summary2/pages/new-meetup/index.js
+++ 01.NextJS_Summary2/pages/new-meetup/index.js
@@ -1,16 +1,34 @@
 import React from 'react'
+import { useRouter } from 'next/router';
 import NewMeetupForm from './../../components/meetups/NewMeetupForm';
 // import Layout from './../../components/layout/Layout';
 
 const NewMeetupPage = () => {
-  function addMeetupHandler(enteredMeetupData) {
-    console.log(enteredMeetupData);
+  const router = useRouter()
+  async function addMeetupHandler(enteredMeetupData) {
+    // api라우트 폴더 내부의 new-meetup으로 경로 설정해서 해당 경로로 POST 요청
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      // JSON 형식으로 변환하여 body에 담음
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+
+    const data = await response.json();
+
+    // console.log(data)
+
+        // replace: 뒤로 가기 금지 
+    // router.replace("/")
+    router.push("/")
   }
-  return(
-  // <Layout>
+  return (
+    // <Layout>
     <NewMeetupForm onAddMeetup={addMeetupHandler} />
-  // </Layout>
-  ) 
+    // </Layout>
+  )
 }
 
 export default NewMeetupPage
