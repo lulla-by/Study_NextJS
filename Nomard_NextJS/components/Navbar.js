0a7fb2468c54ba0145fa88821308646915c009e4
lulla-by <thfl3363@hanmail.net>
2023-08-31 17:02:02 +0900

    CSS Module

--- Nomard_NextJS/components/Navbar.js
+++ Nomard_NextJS/components/Navbar.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import classes from "./Navbar.module.css"
 
 const Navbar = () => {
   const router = useRouter();
   console.log(router.pathname);
   return (
     <nav>
-      <Link href="/" >
-        <span style={{color:router.pathname === "/"?"red":"blue"}}>
+      <Link  href="/" className={`${classes.link} ${router.pathname === "/"?classes.active:""}`} >
+        <span >
           Home
         </span>
       </Link>
-      <Link href="/about" >
-        <span  style={{color:router.pathname === "/about"?"red":"blue"}}>
+      <Link  href="/about" className={`${classes.link} ${router.pathname === "/about" ? classes.active:""}`} >
+        <span >
           About
         </span>
       </Link>
