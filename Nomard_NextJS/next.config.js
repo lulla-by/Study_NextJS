625b2a8b0478ee61b8263fec267876755cf3d693
lulla-by <thfl3363@hanmail.net>
2023-09-04 14:56:59 +0900

    redirect

--- Nomard_NextJS/next.config.js
+++ Nomard_NextJS/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects(){
+    return [
+      {
+        source:"/contact",
+        destination:"https://naver.com",
+        permanent:false
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
